a1c01e35427d8332561a526abd07cb6471fcf314
Daniele Risorgi <risorgid@dhbw-loerrach.de>
2025-02-04 16:53:33 +0100

    wip

--- szi-ki-backend/app.py
+++ szi-ki-backend/app.py
@@ -39,6 +39,7 @@ def chat():
         "user": "user-123"
     }
 
+
     response = requests.post(f"{API_URL}/chat-messages", headers=HEADERS, json=payload)
 
     if response.status_code == 200:
@@ -56,9 +57,33 @@ def chat():
 def health_check():
     return jsonify({"status": "running"})
 
+# BROKE AF
 @app.route('/api/test', methods=['POST'])
 def test():
-    return print("Hello World")
+    data = request.get_json()
+    user_entry = data["user_entry"]
+
+    url = "http://localhost:5000/api"
+
+    payload = {
+        "user_entry": user_entry
+    }
+    response = requests.post(f"{url}/test", headers=HEADERS, json=payload)
+
+    # Überprüfen, ob die Anfrage erfolgreich war
+    if response.status_code != 200:
+        return jsonify({
+            "error": "Fehler bei der Kommunikation mit der externen API",
+            "status_code": response.status_code,
+            "response_text": response.text
+        }), response.status_code
+    try:
+        answer = response.json()
+    except ValueError:
+        return jsonify({"error": "Fehler beim Parsen der API-Antwort."}), 500
+
+    return jsonify({"message": f"Hello World, user says: {answer}"})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
