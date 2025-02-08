8353bf3295f0592f35b025fad693c72d086711aa
Simon Salamun <79207933+sim1sang1songs1@users.noreply.github.com>
2025-02-08 17:02:59 +0100

    Frontend: Switching between chats func

--- szi-ki-frontend/szi-ki-fronted/src/App.tsx
+++ szi-ki-frontend/szi-ki-fronted/src/App.tsx
@@ -6,25 +6,34 @@ import { Chat } from './helper';
 
 
 
-//TODO: If chat empty, show text input in middle, 
+//TODO: state for chats
+//TODO: Get chats from endpoint
+const exampleChats = [
+  new Chat("Chat 1", ["Hello", "How are you?", "I'm fine, thanks!"], new Date('2023-01-01')),
+  new Chat("Chat 1", ["Hello", "How are you?", "I'm fine, thanks!"], new Date('2023-01-01')),
+  new Chat("Chat 1ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", ["Hello", "How are you?", "I'm fine, thanks!"], new Date('2023-01-01')),
+  new Chat("Chat 1", ["Hello", "How are you?", "I'm fine, thanks!"], new Date('2023-01-01')),
+  new Chat("Kaufmännischer Beruf", [
+    "Was macht ein Kaufmann?", 
+    "Ein Kaufmann (oder eine Kauffrau) ist jemand, der im Bereich Handel und Wirtschaft tätig ist und dabei Waren oder Dienstleistungen einkauft, verkauft oder organisiert. Je nach Branche und Spezialisierung können die Aufgaben stark variieren, aber hier sind einige typische Tätigkeiten, die ein Kaufmann ausführt: Ein Kaufmann (oder eine Kauffrau) ist jemand, der im Bereich Handel und Wirtschaft tätig ist und dabei Waren oder Dienstleistungen einkauft, verkauft oder organisiert. Je nach Branche und Spezialisierung können die Aufgaben stark variieren, aber hier sind einige typische Tätigkeiten, die ein Kaufmann ausführt: Ein Kaufmann (oder eine Kauffrau) ist jemand, der im Bereich Handel und Wirtschaft tätig ist und dabei Waren oder Dienstleistungen einkauft, verkauft oder organisiert. Je nach Branche und Spezialisierung können die Aufgaben stark variieren, aber hier sind einige typische Tätigkeiten, die ein Kaufmann ausführt: Ein Kaufmann (oder eine Kauffrau) ist jemand, der im Bereich Handel und Wirtschaft tätig ist und dabei Waren oder Dienstleistungen einkauft, verkauft oder organisiert. Je nach Branche und Spezialisierung können die Aufgaben stark variieren, aber hier sind einige typische Tätigkeiten, die ein Kaufmann ausführt:", 
+    "Achso, und was macht ein Verkaufmann? "
+  ], new Date('2023-02-01')),
+  new Chat("Chat 3", ["Good morning", "Good morning! How did you sleep?", "Very well, thank you."], new Date('2023-03-01')),
+];
 
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const [selectedChat, setSelectedChat] = useState<Chat | null>(null);
-
+  const [selectedChat, setSelectedChat] = useState<Chat>(new Chat("", [], new Date()));
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   }; 
+  console.log('X2 Chat selected in App :', selectedChat);
 
   return (
     <div className={`container ${darkMode ? 'dark' : ''}`}>
-      <Sidebar setSelectedChat={setSelectedChat} toggleDarkmode={toggleDarkMode}/>
-      <ChatComp chat={selectedChat ? selectedChat : new Chat("Kaufmännischer Beruf", [
-    "Was macht ein Kaufmann?", 
-    "Ein Kaufmann (oder eine Kauffrau) ist jemand, der im Bereich Handel und Wirtschaft tätig ist und dabei Waren oder Dienstleistungen einkauft, verkauft oder organisiert. Je nach Branche und Spezialisierung können die Aufgaben stark variieren, aber hier sind einige typische Tätigkeiten, die ein Kaufmann ausführt: Ein Kaufmann (oder eine Kauffrau) ist jemand, der im Bereich Handel und Wirtschaft tätig ist und dabei Waren oder Dienstleistungen einkauft, verkauft oder organisiert. Je nach Branche und Spezialisierung können die Aufgaben stark variieren, aber hier sind einige typische Tätigkeiten, die ein Kaufmann ausführt: Ein Kaufmann (oder eine Kauffrau) ist jemand, der im Bereich Handel und Wirtschaft tätig ist und dabei Waren oder Dienstleistungen einkauft, verkauft oder organisiert. Je nach Branche und Spezialisierung können die Aufgaben stark variieren, aber hier sind einige typische Tätigkeiten, die ein Kaufmann ausführt: Ein Kaufmann (oder eine Kauffrau) ist jemand, der im Bereich Handel und Wirtschaft tätig ist und dabei Waren oder Dienstleistungen einkauft, verkauft oder organisiert. Je nach Branche und Spezialisierung können die Aufgaben stark variieren, aber hier sind einige typische Tätigkeiten, die ein Kaufmann ausführt:", 
-    "Achso, und was macht ein Verkaufmann?"
-  ], new Date('2023-02-01'))} toggleDarkmode={toggleDarkMode}/>
+      <Sidebar setSelectedChat={setSelectedChat} toggleDarkmode={toggleDarkMode} chats={exampleChats}/>
+      <ChatComp chat={selectedChat} toggleDarkmode={toggleDarkMode}/>
     </div>
   );
 }
