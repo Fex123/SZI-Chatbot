fec3c1cb8cf3240f1450b1bcc2cbfe6f3c717030
Fex123 <56225096+Fex123@users.noreply.github.com>
2025-02-19 22:46:23 +0100

    SZI-Chatbot: correct getenv

--- testapi/config.py
+++ testapi/config.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 import os
 
+load_dotenv("../.env")
+
 class Config:
     DEBUG = True
     MONGO_URI = "mongodb://root:example@host.docker.internal:27017/"
     MONGO_DB_NAME = "test_db"
     DIFY_URL = "http://host.docker.internal/v1"
-    DIFY_API_KEY = load_dotenv().get('DIFY_API_KEY')
+    DIFY_API_KEY = os.getenv('DIFY_API_KEY')
     DIFY_HEADERS = {
         'Authorization': f'Bearer {DIFY_API_KEY}',
         'Content-Type': 'application/json'
