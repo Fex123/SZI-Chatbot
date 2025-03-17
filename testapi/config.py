36f50f633d7a4927cbd777521c65f8c3a6a6c981
Fex123 <56225096+Fex123@users.noreply.github.com>
2025-03-18 00:10:48 +0100

    SZI-Chatbot: added gunicorn to set backend in production

--- testapi/config.py
+++ testapi/config.py
@@ -7,7 +7,7 @@ class Config:
     DEBUG = False
     MONGO_URI = "mongodb://root:example@mongo:27017/"
     MONGO_DB_NAME = "dify_mongo"
-    DIFY_URL = "http://docker_nginx_1/v1" # Local: http://docker-nginx-1/v1
+    DIFY_URL = "http://docker_nginx_1/v1" # Local: http://docker-nginx-1/v1  Server: http://docker_nginx_1/v1
     DIFY_API_KEY = os.getenv('DIFY_API_KEY')  
     DIFY_HEADERS = {
         'Authorization': f'Bearer {DIFY_API_KEY}',
